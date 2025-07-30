522ca013b70e08eddd88f582eb8671fd719e8f6e
Alfred Simkin <alfred.simkin@gmail.com>
2025-07-30 17:26:22 -0400

    working snp_eff parser

--- scripts/snpEff_parser.py
+++ scripts/snpEff_parser.py
@@ -119,13 +119,122 @@ def check_unannotated_targeted(vcf_line, targets_dict, depth_dict, samples, muta
 			mutation_number+=1
 	return depth_dict, mutation_number
 
+def grab_mutation_names(sample_dict):
+	'''
+	grabs all the mutation names from the first sample of a sample dictionary
+	'''
+	first_sample=list(sample_dict.keys())[0]
+	return [sample_dict[first_sample][mutation][3] for mutation in sample_dict[first_sample]]
+
+def make_zeroes_dict(samples, missing_muts):
+	'''
+	creates a dictionary assigning 0 coverage, ref, and alt counts to any
+	mutations that were in the targeted mutations but missing from both the
+	snpEff dictionary and the dictionary of targeted mutations that had vcf file
+	coverage.
+	'''
+	zeroes_dict, mut={},1
+	for chrom in targets_dict:
+		for pos in targets_dict[chrom]:
+			for ref in targets_dict[chrom][pos]:
+				for alt in targets_dict[chrom][pos][ref]:
+					if targets_dict[chrom][pos][ref][alt][2] in missing_muts:
+						header=targets_dict[chrom][pos][ref][alt]
+						for sample in samples:
+							zeroes_dict.setdefault(sample, {})
+							zeroes_dict[sample][mut]=['0,0,0']+header
+						mut+=1
+	return zeroes_dict
+
+def remove_redundant(redundant_muts, eff_dict, covered_dict):
+	'''
+	removes any mutations in the covered_dict (vcf calls corresponding to
+	targeted SNPs) that are also annotated already by snpEff - this is necessary
+	because some targeted SNPs are tri-allelic, with the same position
+	associated with two different mutations, only one of which might be observed
+	and therefore annotated by snpEff
+	'''
+	new_covered={}
+	first_sample=list(covered_dict.keys())[0]
+	good_muts=[]
+	for mut in covered_dict[first_sample]:
+		if covered_dict[first_sample][mut][3] not in redundant_muts:
+			for sample in covered_dict:
+				new_covered.setdefault(sample, {})
+				new_covered[sample][mut]=covered_dict[sample][mut]
+	return new_covered
+
+def special_sort(unsorted_list):
+	'''
+	sorts mutations first by gene name and then by numbered amino acid position
+	'''
+	sorting_list=[]
+	for entry in unsorted_list:
+		mutation, number=entry
+		gene='-'.join(mutation.split('-')[:-1])
+		aa_pos=int(mutation.split('-')[-1][3:-3])
+		sorting_list.append([gene, aa_pos, entry])
+	return [item[-1] for item in sorted(sorting_list)]
+
+
+def merge_dicts(eff_dict, nonzero_dict, zero_dict):
+	'''
+	merges together the snp_eff dictionary, the nonzero dictionary, and the zero
+	dictionary. Also creates a sorted list of mutations for easy lookup in the
+	final AA table
+	'''
+	first_sample=list(eff_dict.keys())[0]
+	max_eff=max(eff_dict[first_sample].keys())
+	max_nonzero=max(nonzero_dict[first_sample].keys())
+	sorting_list=[]
+	for mut in eff_dict[first_sample]:
+		sorting_list.append((eff_dict[first_sample][mut][3], mut))
+	merged_dict=eff_dict
+	for sample in nonzero_dict:
+		for mut1 in nonzero_dict[sample]:
+			nonzero_new_mut=max_eff+mut1+1
+			sorting_list.append((nonzero_dict[sample][mut1][3], nonzero_new_mut))
+			merged_dict[sample][nonzero_new_mut]=nonzero_dict[sample][mut1]
+		for mut2 in zero_dict[sample]:
+			zero_new_mut=max_eff+max_nonzero+mut2+2
+			sorting_list.append((zero_dict[sample][mut2][3], zero_new_mut))
+			merged_dict[sample][zero_new_mut]=zero_dict[sample][mut2]
+	sorting_list=list(set(sorting_list))
+	sorted_list=special_sort(sorting_list)
+	print('sorted is', sorted_list)
+	for sample in merged_dict:
+		print(sample, 'merged is', merged_dict[sample].keys())
+	return merged_dict, sorted_list
+
+
+def fill_in_missing_values(protein_dict, targets_dict, targeted_depth_dict, targeted_mutations):
+	'''
+	Examines all targeted_mutations from targets_dict. Any of these targeted
+	mutations that don't have a snpEff annotation (are not in protein_dict) and
+	that don't appear as a reference-only state in the VCF file (are not in
+	targeted_depth_dict) are assumed to be of 0 coverage.
+	TODO:
+	Currently, anything that has a snpEff value and is targeted gets counted
+	twice. Need a check to remove these 'targeted' counts and trust snpEff.
+	'''
+	snp_eff_muts=set(grab_mutation_names(protein_dict))
+	nonzero_muts=set(grab_mutation_names(targeted_depth_dict))
+	targeted_muts=set(targeted_mutations)
+	missing_muts=targeted_muts-(nonzero_muts|snp_eff_muts)
+	redundant_muts=nonzero_muts&snp_eff_muts
+	samples=protein_dict.keys()
+	zeroes_dict=make_zeroes_dict(samples, missing_muts)
+	new_nonzero=remove_redundant(redundant_muts, protein_dict, targeted_depth_dict)
+	merged_dict, sorting_list=merge_dicts(protein_dict, new_nonzero, zeroes_dict)
+	return merged_dict, sorting_list
+
 def parse_vcf_file(input_vcf, targets_dict):
 	'''
 	searches vcf file for snpEff annotations and retrieves the mutation
 	information and depths associated with the snpeff annotations
 	'''
 	ann_dict, target_depth_dict={},{}
-	parsed_counter, mutation_number=0,1
+	mutation_number, parsed_counter=1,0
 	if input_vcf.endswith('.gz'):
 		import gzip
 		file_handle=gzip.open(input_vcf, mode='rt')
@@ -135,17 +244,18 @@ def parse_vcf_file(input_vcf, targets_dict):
 		line=line.strip().split('\t')
 		if line[0].startswith('#CHROM'):
 			samples=line[9:]
-		if len(line)>7 and line[7].split(';')[-1].startswith('ANN'):
-			unparsed_snpeff=line[7].split('|')
-			for sample_number, sample in enumerate(samples):
+		if not line[0].startswith('#'):
+			if line[7].split(';')[-1].startswith('ANN'):
+				unparsed_snpeff=line[7].split('|')
 				for column_number, column in enumerate(unparsed_snpeff):
-					parsed_column_number=column_number%15
-					if parsed_column_number==0:
+					if column_number%15==0:
+						header=[]
 						parsed_counter+=1
+					header.append(column)
+					for sample_number, sample in enumerate(samples):
 						ann_dict.setdefault(sample, {})
-						ann_dict[sample].setdefault(parsed_counter, [line[8]+';'+line[9+sample_number]])
-					ann_dict[sample][parsed_counter].append(column)
-		elif len(line)>7 and not line[0].startswith('#'):
+						if column_number%15==14:
+							ann_dict[sample][parsed_counter]=[line[8]+';'+line[9+sample_number]]+header
 			target_depth_dict, mutation_number=check_unannotated_targeted(line, targets_dict, target_depth_dict, samples, mutation_number)
 	return ann_dict, target_depth_dict
 
@@ -205,32 +315,29 @@ def get_targeted_status(input_tsv, snpeff_dict):
 	snpeff_dictionary is a 'targeted' mutation of interest or not.
 	'''
 
-def format_header(output_file, protein_dict, targeted_depth_dict):
+def format_header(output_file, merged_dict, muts):
 	first_sample=list(protein_dict.keys())[0]
 	header_names=['Gene ID', 'Gene', 'Mutation Name', 'ExonicFunc', 'AA Change', 'Targeted']
 	for row_number, name in enumerate(header_names):
 		output_line=[name]
-		for mut in protein_dict[first_sample]:
-			output_line.append(protein_dict[first_sample][mut][row_number+1])
-		for mut in targeted_depth_dict[first_sample]:
-			output_line.append(targeted_depth_dict[first_sample][mut][row_number+1])
+		for mut in muts:
+			output_line.append(merged_dict[first_sample][mut][row_number+1])
 		output_file.write(','.join(output_line)+'\n')
 
-def output_tables(protein_dict, targeted_depth_dict, output_folder):
+def output_tables(merged_dict, sorting_list, output_folder):
+	muts=[entry[1] for entry in sorting_list]
 	for file_number, output_path in enumerate(['coverage_AA_table.csv', 'reference_AA_table.csv', 'alternate_AA_table.csv']):
 		output_file=open(output_folder+'/'+output_path, 'w')
-		format_header(output_file, protein_dict, targeted_depth_dict)
-		for sample in protein_dict:
+		format_header(output_file, merged_dict, muts)
+		for sample in merged_dict:
 			output_line=[sample]
-			for mut in protein_dict[sample]:
-				output_line.append(protein_dict[sample][mut][0].split(',')[file_number])
-			for mut in targeted_depth_dict[sample]:
-				output_line.append(targeted_depth_dict[sample][mut][0].split(',')[file_number])
+			for mut in muts:
+				output_line.append(merged_dict[sample][mut][0].split(',')[file_number])
 			output_file.write(','.join(output_line)+'\n')
 
 gene_mappings=grab_gene_mappings(input_gff)
 targets_dict, targeted_mutations=make_targets_dict(targets_tsv, gene_mappings)
 ann_dict, targeted_depth_dict=parse_vcf_file(input_vcf, targets_dict)
-print('targeted dict is', targeted_depth_dict)
 protein_dict=parse_annotations(ann_dict, gene_mappings, targeted_mutations)
-output_tables(protein_dict, targeted_depth_dict, 'test_folder')
+merged_dict, sorting_list=fill_in_missing_values(protein_dict, targets_dict, targeted_depth_dict, targeted_mutations)
+output_tables(merged_dict, sorting_list, 'test_folder')
